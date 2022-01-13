addcd6356302eacb12ab8d63d4cecafd0b46f3f0
Gil Carlos <gilcarlos.hermoso@fernando3martos.com>
2022-01-13 10:12:04 +0100

    modifico karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-// Karma configuration file, see link for more information
+// Hola soy Antonio Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function(config) {
